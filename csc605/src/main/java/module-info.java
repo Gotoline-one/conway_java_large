4eab84e4e83978fc9457a0469bc759ad7962b67f
Gotoline-one <103480169+Gotoline-one@users.noreply.github.com>
2025-04-07 16:49:34 -0700

    update to testing.json, launch.json - created more options and better seed for testing -updated module-info.java added transitive statement, and removed extra includes

--- csc605/src/main/java/module-info.java
+++ csc605/src/main/java/module-info.java
@@ -1,14 +1,11 @@
 module com.conway {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.base;
+    requires transitive javafx.controls;
 
     
     exports com.conway ;
     exports com.conway.GameBoard ;
     exports com.conway.MyApp ;
+
     opens com.conway to javafx.controls;
-    opens com.conway.GameBoard to javafx.graphics;
-    opens com.conway.MyApp to javafx.graphics;
     
 }
