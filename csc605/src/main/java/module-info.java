d3001fe65927851559b9fc12de020eb344d98569
Gotoline-one <103480169+Gotoline-one@users.noreply.github.com>
2025-04-07 19:47:56 -0700

    update module-info again to remove wanings

--- csc605/src/main/java/module-info.java
+++ csc605/src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module com.conway {
     requires transitive javafx.controls;
     requires javafx.graphics;
-
     
-    exports com.conway ;
+    exports com.conway.AppOptions ;
     exports com.conway.GameBoard ;
     exports com.conway.MyApp ;
-    exports com.conway.AppOptions ;
+    exports com.conway.Utilities ;
+    exports com.conway ;
 
     opens com.conway to javafx.controls;
-    
 }
